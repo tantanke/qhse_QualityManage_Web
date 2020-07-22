41a4461ea6d0a1c26fde96e05857ad5136d5d88d
Forever-Zhang <747005221@qq.com>
2020-07-22 08:38:43 +0800

    隐患接口文件补充

---
+++ safety_web/src/services/hidden_danger_investigation/QHSETroubleCheckTable.js
@@ -0,0 +1,35 @@
+import request from '../../utils/request'
+
+export const GetCheckRecordTree = () => {
+    return request('/api/check_record_tree', {
+        method: 'get'
+    })
+  }
+export const GetqhseCompanytree = () => {
+  return request('/api/qhse_company/tree', {
+      method: 'get'
+  })
+}
+export const getChecklistTree = () => {
+  return request('/api/check_list_tree', {
+      method: 'get'
+  })
+}
+export const addCheckRecord = (data) => {
+  return request('/api/check_record', {
+      method: 'post',
+      data
+  })
+}
+export const editCheckRecord = (data) => {
+  return request('/api/check_record/'+ data.id, {
+      method: 'put',
+      data
+  })
+}
+export const deleteCheckRecord = (data) => {
+  return request('/api/check_record/' + data.id, {
+      method: 'delete',
+      data
+  })
+}
