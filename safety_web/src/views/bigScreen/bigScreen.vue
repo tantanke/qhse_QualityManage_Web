89c3444acf93baff6bb35cd6da96624af21d8f9e
tandake <1552832259@qq.com>
2020-10-26 14:22:07 +0800

    部分修改

--- safety_web/src/views/bigScreen/bigScreen.vue
+++ safety_web/src/views/bigScreen/bigScreen.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="main" :style ="note">
-
-</div>
+  <el-row class="main" :style ="bg">
+     <el-row class="head">
+          <el-col class="title"  :span='5'>
+            <p>安检院QHSE看板</p>
+          </el-col>
+          <el-col class="time"  :span='3'>
+            <span>时间：{{nowTime}}</span>
+          </el-col>
+          <el-col class="buttons" :span='4'>
+            <el-col :span='10'><i class="el-icon-arrow-right"></i><span>安全管理</span></el-col>
+            <el-col :span='9'><i class="el-icon-arrow-right"></i><span>环保管理</span></el-col>
+          </el-col>
+     </el-row>
+     <el-row class="main-item1">
+      <el-row class="rightContent">
+      <el-row class="title">222</el-row>
+      <el-row class="qualityManage"  :style="quality">    
+      </el-row>
+    </el-row>
+     </el-row>
+</el-row>
 
 </template>
 
@@ -9,13 +27,65 @@
 export default {
   data() {
  return {
-  note: {
+  // 背景样式
+  bg: {
   backgroundImage: "url(" + require("./img/bg.jpg") + ")",
   backgroundRepeat: "no-repeat",
-  backgroundSize:"100% 100%"
+  backgroundSize:"100% 100%",
   },
+  // 质量管理背景
+  quality: {
+  backgroundImage: "url(" + require("./img/hdbj.png") + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundSize:"100% 100%",
+  },
+  // 记录管理
+  recording: {
+  backgroundImage: "url(" + require("./img/hdbj.png") + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundSize:"100% 100%",
+  },
+  time:'加载中...'
  }
-  }
+  },
+  methods: {
+    updateTime(){
+      setInterval(()=>{
+       let dd=new Date;
+        //获取年月日，时分秒
+        let y = dd.getFullYear();
+        let m = dd.getMonth()+1;
+        if(Number(m)<10){
+          m = '0'+ m
+        }
+        let d = dd.getDate();
+        if(Number(d)<10){
+          d = '0'+ d
+        }
+        let h = dd.getHours();
+        if(Number(h)<10){
+          h = '0'+ h
+        }
+        let i = dd.getMinutes();
+        if(Number(i)<10){
+          i = '0'+ i
+        }
+        let s = dd.getSeconds();
+        if(Number(s)<10){
+          s = '0'+ s
+        }
+        this.time = y+"-"+m+"-"+d+"   "+h+":"+i+":"+s
+      },1000)   
+    }
+  },
+  computed: {
+    nowTime() {
+      return this.time
+    }
+  },
+  mounted() {
+    this.updateTime()
+  },
 }
 </script>
 
