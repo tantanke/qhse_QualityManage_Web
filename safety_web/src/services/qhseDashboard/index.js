c7253438f61582e9031ad989853e22734ad0fbfc
tandake <1552832259@qq.com>
2020-11-02 20:45:45 +0800

    文件下载+大屏数据

--- safety_web/src/services/qhseDashboard/index.js
+++ safety_web/src/services/qhseDashboard/index.js
@@ -1,27 +1,31 @@
 // 查询相关接口
 import request from '@/utils/request'
+import doloadFile from '@/utils/doloadFile'
 
 // 查询质量管理
-export const queryDashboardQualityManagement = (params) => {
-    return request('/api/queryDashboardQualityManagement', {
-      params
-    })
+export const queryDashboardQualityManagement = (data) => {
+  return request('/api/queryDashboardQualityManagement?', {
+    method:'post',
+    data
+  })
   }
 
 
 // 查询记录仪数据
-export const queryDashboardRecorderManagement = (params) => {
+export const queryDashboardRecorderManagement = (data) => {
     return request('/api/queryDashboardRecorderManagement', {
-      params
+      method:'post',
+      data
     })
   }
 
 
 // 查询标准进度管理
-export const queryDashboardScheduleManagement = (params) => {
-    return request('/api/queryDashboardScheduleManagement', {
-      params
-    })
+export const queryDashboardScheduleManagement = (data) => {
+  return request('/api/queryDashboardScheduleManagement', {
+    method:'post',
+    data
+  })
   }
 //查询公司信息
   export const GetqhseCompanytree = () => {
@@ -30,10 +34,36 @@ export const queryDashboardScheduleManagement = (params) => {
     })
   }
 // 下载相关接口
-
-// 下载质量管理
-// 下载记录仪数据
 // 下载标准进度管理
+export const downloadDashboardScheduleManagementTemplate = () => {
+  return doloadFile('/api/downloadDashboardScheduleManagementTemplate', {
+      method: 'get'
+  })
+}
+// 下载质量管理
+export const downloadDashboardQualityManagementTemplate = () => {
+  return doloadFile('/api/downloadDashboardQualityManagementTemplate', {
+      method: 'get'
+  })
+}
+// 下载记录仪数据
+export const downloadDashboardRecorderManagementTemplate = () => {
+  return doloadFile('/api/downloadDashboardRecorderManagementTemplate', {
+      method: 'get'
+  })
+}
+// 下载相关函数 
+export const downloadfile = (filename,blob) => {
+                const fileName = filename + ".xls";
+                const elink = document.createElement("a"); // 创建的标签
+                elink.download = fileName;
+                elink.style.display = "none";
+                elink.href = URL.createObjectURL(blob); // 创建url
+                document.body.appendChild(elink); // 把 创建的标签追加到body里
+                elink.click();
+                URL.revokeObjectURL(elink.href); // 释放URL 对象
+                document.body.removeChild(elink); // 移除标签
+}
 
 // 上传相关接口
 
--- safety_web/src/views/qhse_QualityManage/qhse_BigScreenManage/progress.vue
+++ safety_web/src/views/qhse_QualityManage/qhse_BigScreenManage/progress.vue
@@ -22,7 +22,10 @@
                     <el-button type='primary' icon="el-icon-search" @click="searchData">查询</el-button>
                 </el-form-item>
                 <el-form-item>
-                  <el-button type='success' icon="el-icon-upload2" @click="importFile">数据导入</el-button>
+                   <el-upload ref="upload" action='/api/uploadDashboardScheduleManagement'  :headers="headers" :show-file-list="false"
+               accept=".excel, .xls, .xlsx" :on-progress="handleProgress">
+						<el-button icon="el-icon-upload" type="success" @click="importFile">数据导入</el-button>
+					</el-upload>
                 </el-form-item>
                 <el-form-item>
                   <el-button type='info' icon="el-icon-download" @click="downLoadFile">模板下载</el-button>
@@ -32,6 +35,8 @@
             <el-row style="padding:10px; border-top: 2px dashed #dddddd;text-align:center">
                   <el-table
                         :data="listData"
+                        v-loading='loading'
+                        element-loading-text="查询中"
                         style="width: 100%;text-align:center"
                         ref="treeTable"
                         row-key="code"
@@ -39,6 +44,15 @@
                         max-height="560"
                         highlight-current-row
                         border>
+                        <el-table-column align='center' prop='companyName' label='单位名称'></el-table-column>
+                        <el-table-column align='center' prop='planNum' label='初稿计划数数'></el-table-column>
+                        <el-table-column align='center' prop='firstDraftFinishNum' label='初稿完成数'></el-table-column>
+                        <el-table-column align='center' prop='firstDraftFinishRate' label='初稿完成率'></el-table-column>
+                        <el-table-column align='center' prop='ReviewPassNum' label='评审通过数'></el-table-column>
+                        <el-table-column align='center' prop='ReviewPassRate' label='评审通过率'></el-table-column>
+                        <el-table-column align='center' prop='StandardReleaseNum' label='标准发布数'></el-table-column>
+                        <el-table-column align='center' prop='StandardReleaseRate' label='标准发布率'></el-table-column>
+                        <el-table-column align='center' prop='updateTime' label='填报时间'></el-table-column>
                       </el-table>
             </el-row> 
       </div>
@@ -47,45 +61,68 @@
 
 <script>
 import CurrentUser from '@/store/CurrentUser'
-import {queryDashboardScheduleManagement,GetqhseCompanytree} from '@/services/qhseDashboard/index'
+import {queryDashboardScheduleManagement,GetqhseCompanytree,downloadDashboardScheduleManagementTemplate,downloadfile} from '@/services/qhseDashboard/index'
 export default {
     data() {
       return {
         selectdate: '',
         chooseItem:'',
         companyId:'',
+        searchForm:{companyCode:''},
         companyList:[],
-        listData:[]
+        listData:[],
+        loading:false,
+        headers:{Authorization:CurrentUser.get().token}
       }
     },
     methods: {
       searchData(){
-        console
+        this.loading = true
+        queryDashboardScheduleManagement(this.searchForm).then(res => {
+          this.listData = res.data
+           this.loading = false
+         console.log(res.data)
+        }).catch(() => {
+           this.loading = false
+          this.$message.error("查询失败，请稍后再试！")
+        })
       },
+      getUrl(baseurl,data) {
+            let url = baseurl+'?'
+            Object.keys(data).forEach( (key)=> {
+                 url =  `${url}${key}=${data[key]}&`        
+                });
+                url = url.substring(0,url.length-1)
+            return url
+        },
       importFile(){
 
       },
       downLoadFile(){
+        downloadDashboardScheduleManagementTemplate().then(res =>{
+          downloadfile('标准进度管理模板',res.file.data)
 
+        })
       },
        getCompany(){
         GetqhseCompanytree().then(res =>{
                this.companyList = res.data
-        }).catch(err =>{
-          this.$message.error(err)
+        }).catch(() =>{
+          this.$message.error("查询失败，请稍后再试！")
+          
         })
         
       },
       handleChange(){
-        this.companyId = this.chooseItem[this.chooseItem.length - 1]
-        console.log(this.companyId)
-      }
+        this.searchForm.companyCode = this.chooseItem[this.chooseItem.length - 1]    
+      },
+      handleProgress(){
+            this.$alert('文件上传中，请稍候', '文件上传')
+        },
+      
     },
     mounted() {
       this.getCompany()
-      queryDashboardScheduleManagement().then(res => {
-        console.log(res)
-      })
     },
 }
 </script>
