d38aa1bffcab090e758d249bfc8427ebb1edd8a6
tanlei <384221465@qq.com>
2020-11-07 16:07:56 +0800

    feat:改动了一点点

--- safety_web/src/views/remote_Moniter/RemotePlanManager/compoments.vue
+++ safety_web/src/views/remote_Moniter/RemotePlanManager/compoments.vue
@@ -192,17 +192,18 @@
 
             confirmCheckIn() {
                 // 确认录入
-                console.log(this.resData, 555555)
+
                 this.resData.monitorPlanID = this.$route.params.monitorPlanID
-                // 等后端的接口了
+                console.log(this.resData, '确认录入的表单数据')
                 createNewDetail(this.resData).then(res=>{
-                    console.log(res, 384)
+                    this.$message.success("手动录入成功")
                     getDetails(this.$route.params).then(res => {
                         this.listData = res.data;
                         this.filterMethods(this.listData)
                     })
+
                 }).catch(err=>{
-                    console.log(err, 2214)
+                    this.$message.error("手动录入失败")
                 })
                 this.isCheckIn = false;
             }
