d2c1c6b2445f99ea960b3603e56654eb54152c08
tanlei <384221465@qq.com>
2020-11-12 13:33:19 +0800

    feat: 远程监控模块

--- safety_web/src/views/remote_Moniter/RemotePlanStatistics/planData.vue
+++ safety_web/src/views/remote_Moniter/RemotePlanStatistics/planData.vue
@@ -38,6 +38,7 @@
                                     <el-table-column prop="inputPersonName" label="监控人员" align="center"></el-table-column>
                                     <el-table-column prop="inputDate" label="录入时间" align="center"></el-table-column>
                                     <el-table-column prop="checkDate" label="核查时间" align="center"></el-table-column>
+                                    <el-table-column prop="result" label="核查结论" align="center"></el-table-column>
                                     <el-table-column prop="closeInOrCheck" label="是否关闭" align="center"></el-table-column>
                                 </el-table>
                             </el-form>
