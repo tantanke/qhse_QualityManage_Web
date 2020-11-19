bd5f6c981e4f95bed7bde6cbd34c3973bf1092f6
tanlei <384221465@qq.com>
2020-11-19 13:01:24 +0800

    feat: 修改查核状态显示位置

--- safety_web/src/views/remote_Moniter/RemotePlanCheck/index.vue
+++ safety_web/src/views/remote_Moniter/RemotePlanCheck/index.vue
@@ -19,13 +19,13 @@
       <el-row style="padding:10px; border-top: 2px dashed #dddddd;text-align:center">
         <el-table :data="listData" style="width: 100%;text-align:center" ref="treeTable" row-key="code" :indent="30"
           max-height="560" highlight-current-row border
-          :cell-style="cellStyle"
+
         >
           <el-table-column type="index" label="序号" width="60" align="center" show-overflow-tooltip></el-table-column>
           <el-table-column prop="planName" label="计划名称" align="center" show-overflow-tooltip> </el-table-column>
           <el-table-column prop="startDate" label="开始时间" align="center" show-overflow-tooltip> </el-table-column>
           <el-table-column prop="endDate" label="结束时间" align="center" show-overflow-tooltip> </el-table-column>
-          <el-table-column prop="checkStatus" label="核查状态" align="center" show-overflow-tooltip> </el-table-column>
+
           <!-- <el-table-column label="操作" width="100" align="center" show-overflow-tooltip>
             <template slot-scope="scope">
               <el-button v-if="ifcanwrite(scope.row)" type="primary" size="mini" @click="readfile(scope.row)" icon="el-icon-edit">核查
@@ -94,13 +94,7 @@ export default {
     }
   },
   methods: {
-    cellStyle(row, column, rowIndex, columnIndex) {//根据报警级别显示颜色
-        if ( row.column.label==='核查状态' && row.row.checkStatus === "未核查") {
-            return 'color:red'
-        } else {
-            return ''
-        }
-    },
+
     inputdaily () {
       getDayReport({ monitorPlanID: this.monitorPlanID, date: this.selecttime }).then(res => {
         console.log(res.data)
