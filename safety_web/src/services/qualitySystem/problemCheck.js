3d01470384d9597c09b7960787ae3c377575b707
Hengbin-zhang <747005221@qq.com>
2021-03-29 10:37:56 +0800

    更新问题审核查询接口

--- safety_web/src/services/qualitySystem/problemCheck.js
+++ safety_web/src/services/qualitySystem/problemCheck.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 // 查询表单
 export const inquireProblemReviewForm =(data)=>{
-	return request(`/api/queryByYearComAndModify?checkedCompanyCode=${data.checkedCompanyCode}&checkDate=${data.checkDate}`,{method:'get',data})
+	return request(`/api/queryByYearComAndModify?checkedCompanyCode=${data.checkedCompanyCode}&checkDate=${data.checkDate}&checkEndDate=${data.checkEndDate}`,{method:'get',data})
 }
 // 获取所有的基本信息登记表
 export const getBasicInfomation =()=>{
